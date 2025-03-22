44d310d5a271de944158659b2d5c7724f90bf73c
Augusto Mangini <augustomangini99@gmail.com>
2025-03-22 16:54:40 -0300

    Criando Dockerfile

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +14,12 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	var host = os.Getenv("HOST")
+	var user = os.Getenv("USER")
+	var password = os.Getenv("PASSWORD")
+	var name = os.Getenv("DBNAME")
+	var port = os.Getenv("PORT")
+	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
